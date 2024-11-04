156adcbca01200a2c96aa829b4e6194fef8e0f59
Jeanne <jeannefernanda.m@gmail.com>
2024-11-04 17:19:29 -0400

    Implementando Beans Validation

--- api/src/main/java/med/voll/api/endereco/DadosEndereco.java
+++ api/src/main/java/med/voll/api/endereco/DadosEndereco.java
@@ -1,4 +1,18 @@
 package med.voll.api.endereco;
 
-public record DadosEndereco(String logradouro, String bairro, String cep, String cidade, String uf, String complemento, String numero) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record DadosEndereco(String logradouro,
+                            @NotBlank
+                            String bairro,
+                            @NotBlank
+                            @Pattern(regexp = "\\d{8}")
+                            String cep,
+                            @NotBlank
+                            String cidade,
+                            @NotBlank
+                            String uf,
+                            String complemento,
+                            String numero) {
 }
