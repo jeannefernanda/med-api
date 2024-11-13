b7cb31f6e4f90d0b03ef5db40d12b85b3316fb4a
Jeanne <jeannefernanda.m@gmail.com>
2024-11-13 18:30:36 -0400

    povoando tabela de pacientes

---
+++ api/src/main/resources/db/migration/V7__insert-into table-pacientes.sql
@@ -0,0 +1,6 @@
+INSERT INTO pacientes (
+    nome, email, cpf, telefone, logradouro, bairro, cep, complemento, numero, uf, cidade, ativo
+) VALUES
+    ('João da Silva', 'joao.silva@email.com', '12345678900', '98765-321', 'Rua das Flores', 'Centro', '12345-678', 'Apto 101', '100', 'SP', 'São Paulo', 1),
+    ('Maria Oliveira', 'maria.oliveira@email.com', '98765432100', '976543210', 'Avenida Brasil', 'Copacabana', '98765-432', 'Casa 5', '200', 'RJ', 'Rio de Janeiro', 1),
+    ('Carlos Pereira', 'carlos.pereira@email.com', '12378945600', '965432109', 'Rua do Sol', 'Bairro Novo', '65432-109', 'Sala 301', '300', 'MG', 'Belo Horizonte', 1);
