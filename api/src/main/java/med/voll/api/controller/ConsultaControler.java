90685a58f546cbd287a0ddf8cdcc3d35c6b0a76e
Jeanne <jeannefernanda.m@gmail.com>
2024-11-22 19:30:52 -0400

    Criando a funcionalidade agendamento de consulta

---
+++ api/src/main/java/med/voll/api/controller/ConsultaControler.java
@@ -0,0 +1,18 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.consulta.DadosAgendamentoConsulta;
+import med.voll.api.consulta.DadosDetalhamentoConsulta;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("consultas")
+public class ConsultaControler {
+    public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados){
+        System.out.println(dados);
+        return ResponseEntity.ok(new DadosDetalhamentoConsulta(null, null, null, null));
+    }
+}
