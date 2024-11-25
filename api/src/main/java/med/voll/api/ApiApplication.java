5072222533e39e65bd31f4827a2ce466da974e5c
Jeanne <jeannefernanda.m@gmail.com>
2024-11-25 17:21:08 -0400

    Criando a Classe Service

--- api/src/main/java/med/voll/api/ApiApplication.java
+++ api/src/main/java/med/voll/api/ApiApplication.java
@@ -1,10 +1,12 @@
 package med.voll.api;
 
+import med.voll.api.domain.consulta.ConsultaRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
