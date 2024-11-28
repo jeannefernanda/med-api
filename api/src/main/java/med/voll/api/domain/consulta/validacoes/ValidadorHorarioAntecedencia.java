63c7ef8f1f3f7a3df57210408cb26346e5fdb1a8
Jeanne <jeannefernanda.m@gmail.com>
2024-11-27 21:11:53 -0400

    Aplicando Principios SOLID

--- api/src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioAntecedencia.java
+++ api/src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioAntecedencia.java
@@ -2,11 +2,14 @@ package med.voll.api.domain.consulta.validacoes;
 
 import med.voll.api.domain.ValidacaoException;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
 
+@Component
 public class ValidadorHorarioAntecedencia implements ValidadorAgendamentoDeConsulta{
+
 
     public void validar(DadosAgendamentoConsulta dados){
         var dataConsulta = dados.data();
